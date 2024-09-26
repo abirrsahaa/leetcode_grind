027de9275655b55afdf8a8cf832975278639e521
Abir Saha <112876200+abirrsahaa@users.noreply.github.com>
2024-09-26 15:17:13 +0530

    Time: 4 ms (72.75%), Space: 10.5 MB (71.65%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -44,6 +44,12 @@ public:
         string ans="";
         while(!track.empty()){
             string pehla=track.top();
+            // here i will make mistake as i have not visualised it 
+            // the string in stack are in desired order 
+            // and i am making the ans string in reverse order 
+            // so in order to reverse the string will also be reversed 
+            // so it is liable to insert this string into my ans stack 
+            // by reversing it 
             reverse(pehla.begin(),pehla.end());
             track.pop();
             // pehla=pehla+'/';
