ce1bf474cfa96b45e37283979c8de3d0fc6f85cc
Abir Saha <112876200+abirrsahaa@users.noreply.github.com>
2024-09-28 17:33:48 +0530

    Time: 0 ms (100%), Space: 11.7 MB (45.14%) - LeetHub

--- 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.cpp
@@ -10,43 +10,33 @@
  * };
  */
 
-//  lets first discuss the algo coming into mind 
-// first 
-// traverse through all the nodes get the value and then store it in array 
-// sort it and get the min diff in 0(n);
-
-
-// else we cannot get the two smaller number at that doesnt ensure it 
-// i am going witn inorder and then o(n);
-
-// OPTIMIZATION-->
-// what we can do is in inorder when we are backtracking we can calculate the min diff
-// so as to reduce the another iteration and aslo to sve in space 
-
+//  previously i used a o(n) space and another 0(n) iteration 
+// this apaproach discards both of this and calculates it on the fly 
+// although the intuition still remains same it is just how we 
+// optimize our code further 
 class Solution {
 public:
-    void helper(TreeNode* root,vector<int>&track){
-        if(!root->left&&!root->right){
-            track.push_back(root->val);
-            return;
-        }
-        if(root->left)helper(root->left,track);
-        // cout<<"data"<<root->val<<endl;
-        track.push_back(root->val);
-        if(root->right)helper(root->right,track);
-    }
     int minDiffInBST(TreeNode* root) {
-
-        int diff=INT_MAX;
-        vector<int>track;
-        helper(root,track);
-        // cout<<"printing the inorder"<<endl;
-        // for(auto i:track)cout<<i<<" ";
-        // cout<<endl;
-        for(int i=1;i<track.size();i++){
-            diff=min(diff,abs(track[i]-track[i-1]));
-        }
-        return diff;
+        int diff = INT_MAX;
+        TreeNode* prev = nullptr;
         
+        helper(root, prev, diff);
+        
+        return diff;
+    }
+
+    void helper(TreeNode* node, TreeNode*& prev, int& diff) {
+        if (!node) return;
+        
+        // In-order traversal (left -> node -> right)
+        helper(node->left, prev, diff);
+        
+        if (prev != nullptr) {
+            diff = min(diff, node->val - prev->val);
+        }
+        
+        prev = node; // Update the previous node
+        
+        helper(node->right, prev, diff);
     }
 };
