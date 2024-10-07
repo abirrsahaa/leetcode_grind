39afcb3be3c708d22bc4ade157a1ea209b2185df
Abir Saha <112876200+abirrsahaa@users.noreply.github.com>
2024-10-07 20:32:53 +0530

    Time: 3 ms (29.55%), Space: 7.7 MB (9.42%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -7,9 +7,21 @@ public:
         return dp[n]=memo(n-1,dp)+memo(n-2,dp);
 
     }
+    int iterative(int n){
+        if(n==0||n==1||n==2)return n;
+        vector<int>dp(n+1,0);
+        dp[1]=1;
+        dp[2]=2;
+        // if()
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return memo(n,dp);
+        // return memo(n,dp);
+        return iterative(n);
         
     }
 };
