3adc825e698ccc748d932906efcd00aee9893181
Abir Saha <112876200+abirrsahaa@users.noreply.github.com>
2024-10-16 18:34:56 +0530

    Time: 3 ms (54.09%), Space: 10.7 MB (6.53%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -19,6 +19,34 @@ public:
         return dp[row][col]= memo(obstacleGrid,row-1,col,dp)+memo(obstacleGrid,row,col-1,dp);                
     }
 
+    // lets come to tabulation
+    int tabulation(vector<vector<int>>& obstacleGrid,int row,int col){
+        if(obstacleGrid[0][0])return 0;
+        vector<vector<int>>dp(obstacleGrid.size()+1,vector<int>(obstacleGrid[0].size()+1,0));
+
+        for(int i=0;i<obstacleGrid.size();i++){
+            if(obstacleGrid[i][0])break;
+            dp[i][0]=1;
+        }
+
+        for(int i=0;i<obstacleGrid[0].size();i++){
+            if(obstacleGrid[0][i])break;
+            dp[0][i]=1;
+        }
+
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+
+        // formulating the tabulation
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j]!=1)dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                else dp[i][j]=0;
+            }
+        }
+        return dp[m-1][n-1];
+    }
+
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
 
@@ -27,7 +55,8 @@ public:
 
         // return helper(obstacleGrid,m-1,n-1);
         vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return memo(obstacleGrid,m-1,n-1,dp);
+        // return memo(obstacleGrid,m-1,n-1,dp);/
+        return tabulation(obstacleGrid,m,n);
         
     }
 };
