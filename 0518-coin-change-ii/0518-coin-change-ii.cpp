fe2163ed7da3d3ea35ee4caac49c85071ecd817f
Abir Saha <112876200+abirrsahaa@users.noreply.github.com>
2024-10-18 22:32:10 +0530

    Time: 43 ms (10.47%), Space: 27.5 MB (16.9%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int helper(vector<int>&coins,int amount,int index,vector<vector<int>>&dp){
+
+
+
+// that is why it is suggested that always formulate the top_down in the top-down format
+// there is a reason why it is said so 
+        // these are my base cases 
+        if(amount==0)return 1;
+        if(index==coins.size()){
+            if(amount==0)return 1;
+            return 0;
+        }
+        if(amount<0)return 0;
+
+
+        if(dp[index][amount]!=-1)return dp[index][amount];
+
+        // now is the time for my choices
+        int take=helper(coins,amount-coins[index],index,dp);
+        // if(take!=INT_MAX)take+=1;
+        int not_take=helper(coins,amount,index+1,dp);
+        return dp[index][amount]= take+not_take;
+    }    
+    int change(int amount, vector<int>& coins) {
+
+
+        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
+
+
+        return helper(coins,amount,0,dp);
+        
+    }
+};
