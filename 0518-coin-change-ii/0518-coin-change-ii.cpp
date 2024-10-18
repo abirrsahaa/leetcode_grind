22f63d80dec4bbe1651509dbef999ceaf718285b
Abir Saha <112876200+abirrsahaa@users.noreply.github.com>
2024-10-18 22:36:14 +0530

    Time: 19 ms (34.95%), Space: 27.3 MB (21.49%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -8,8 +8,8 @@ public:
 // there is a reason why it is said so 
         // these are my base cases 
         if(amount==0)return 1;
-        if(index==coins.size()){
-            if(amount==0)return 1;
+        if(index==0){
+            if((amount%coins[0])==0)return 1;
             return 0;
         }
         if(amount<0)return 0;
@@ -20,7 +20,7 @@ public:
         // now is the time for my choices
         int take=helper(coins,amount-coins[index],index,dp);
         // if(take!=INT_MAX)take+=1;
-        int not_take=helper(coins,amount,index+1,dp);
+        int not_take=helper(coins,amount,index-1,dp);
         return dp[index][amount]= take+not_take;
     }    
     int change(int amount, vector<int>& coins) {
@@ -29,7 +29,7 @@ public:
         vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
 
 
-        return helper(coins,amount,0,dp);
+        return helper(coins,amount,coins.size()-1,dp);
         
     }
 };
