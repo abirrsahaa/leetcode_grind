5ec810db57c5162fffc88a800a538f1986e93de9
Abir Saha <112876200+abirrsahaa@users.noreply.github.com>
2024-10-22 15:27:33 +0530

    Time: 590 ms (5.06%), Space: 295.2 MB (5.59%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -0,0 +1,21 @@
+// i am fucking surprised to know the fact that 
+// i havent solved this problem 
+
+class Solution {
+public:
+    int helper(vector<int>&nums,int index,int prev,vector<vector<int>>&dp){
+        if(index==nums.size())return 0;
+        if(dp[index][prev+1]!=-1)return dp[index][prev+1];
+
+        int not_taken=0+helper(nums,index+1,prev,dp); //agr nhi liya toh 
+        int taken=0;
+        if(prev==-1||nums[index]>nums[prev]){
+            taken=1+helper(nums,index+1,index,dp); //agr liya toh
+        }
+        return dp[index][prev+1]= max(not_taken,taken);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
+        return helper(nums,0,-1,dp);
+    }
+};
