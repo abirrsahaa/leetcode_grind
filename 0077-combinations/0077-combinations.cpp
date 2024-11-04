872d833b5b64c7a6666de0e7151c450788228d2f
Abir Saha <112876200+abirrsahaa@users.noreply.github.com>
2024-11-04 10:11:37 +0530

    Time: 89 ms (32.36%), Space: 106.4 MB (36.43%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void helper(vector<int>&temp,vector<vector<int>>&ans,int index,int k,int n){
+        if(temp.size()==k){
+            ans.push_back(temp);
+            return;
+        }
+        if(index>n)return;
+        // include 
+        temp.push_back(index);
+        helper(temp,ans,index+1,k,n);
+        temp.pop_back();
+        helper(temp,ans,index+1,k,n);
+        // exclude
+    }
+    vector<vector<int>> combine(int n, int k) {
+
+        // i would be needing 
+        // a answer vector , a temp vector, a index 
+        vector<int>temp;
+        vector<vector<int>>ans;
+        helper(temp,ans,1,k,n);
+        return ans;
+        
+    }
+};
